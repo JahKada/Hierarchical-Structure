5de8811ea41717d148c310aba413ce76e2c17706
JahKada <111883246+JahKada@users.noreply.github.com>
2023-04-16 03:51:15 +0200

    Add files via upload

---
+++ NodeTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastTry
+{
+    public class NodeTree
+    {
+        public int Value { get; set; }
+        public List<NodeTree> Children { get; set; }
+
+        public NodeTree(int value)
+        {
+            Value = value;
+            Children = new List<NodeTree>();
+
+        }
+
+        public void AddChild(NodeTree child)
+        {
+            Children.Add(child);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastTry
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            NodeTree root = new NodeTree(1);
+            NodeTree child1 = new NodeTree(2);
+            NodeTree child2 = new NodeTree(0);
+            NodeTree grandChild1 = new NodeTree(0);
+            NodeTree grandChild2 = new NodeTree(0);
+            NodeTree grandChild3 = new NodeTree(0);
+            NodeTree grandChild4 = new NodeTree(0);
+            NodeTree GgrandChild1= new NodeTree(4);
+            NodeTree GgrandChild2 = new NodeTree(0);
+            NodeTree GgrandChild3 = new NodeTree(0);
+            NodeTree GggrandChild1 = new NodeTree(3);
+
+            root.AddChild(child1);
+            root.AddChild(child2);
+            child1.AddChild(grandChild1);
+            child2.AddChild(grandChild2);
+            child2.AddChild(grandChild3);
+            child2.AddChild(grandChild4);
+            grandChild2.AddChild(GgrandChild1);
+            grandChild3.AddChild(GgrandChild2);
+            grandChild3.AddChild(GgrandChild3);
+            GgrandChild2.AddChild(GggrandChild1);
+
+            int depth = CalDepth(root);
+            Console.WriteLine("Depth of provided structure is: " + depth);
+
+        }
+
+        private static int CalDepth(NodeTree node)
+        {
+            if (node.Children.Count == 0)
+            {
+                return 0;
+            }
+
+            int maxDepth = 0;
+
+            foreach (NodeTree child in node.Children)
+            {
+                int childDepth = CalDepth(child);
+                if (childDepth > maxDepth)
+                {
+                    maxDepth = childDepth;
+                }
+
+            }
+
+            return maxDepth + 1;
+        }
+    }
+}
